da1fb1a4665b75d7a69b106835b53d517f51bdb5
Robbie Le Grange <robbie.legrange@gmail.com>
2022-10-19 23:13:29 +1100

    Added Bot startup using discord-py

--- robot.py
+++ robot.py
@@ -1,4 +1,17 @@
-import hikari
+import discord
 
-bot = hikari.GatewayBot(token='MTAzMjA0MTAxMDIyMzY0ODc5OA.GPgesj.bM8Qg19LcmmVXLsjuvGtyh292HUEaetJjhuFtI')
-bot.run()
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = '!', intents = discord.Intents.all())
+
+
+@client.event
+async def on_ready():
+    print("Robot is ready!")
+    print("---------------")
+
+@client.command()
+async def hello(ctx):
+    await ctx.send("Hello, I am Robot!")
+
+client.run('MTAzMjA0MTAxMDIyMzY0ODc5OA.GPgesj.bM8Qg19LcmmVXLsjuvGtyh292HUEaetJjhuFtI')
