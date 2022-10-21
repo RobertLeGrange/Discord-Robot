d5aa8ce3583a5f3e42da60dba7ea2afcd554fb34
Robbie Le Grange <robbie.legrange@gmail.com>
2022-10-22 08:23:00 +1100

    Added !games function

--- robot.py
+++ robot.py
@@ -1,4 +1,5 @@
 import discord
+from datetime import datetime as dt
 
 from discord.ext import commands
 
@@ -13,5 +14,9 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send("Hello, I am Robot!")
+
+@client.command()
+async def games(ctx):
+    await ctx.send("<@191661345794424832> <@329167323510603786> <@537543052982485013> it gaem time. \nSincerely, Robot")
 
 client.run('MTAzMjA0MTAxMDIyMzY0ODc5OA.GPgesj.bM8Qg19LcmmVXLsjuvGtyh292HUEaetJjhuFtI')
