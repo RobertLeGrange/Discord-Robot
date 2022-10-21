9a71298b571e17d77eb656d40585f68e08427ded
Robbie Le Grange <robbie.legrange@gmail.com>
2022-10-22 08:56:20 +1100

    Updated !mel_time function to include all times

--- robot.py
+++ robot.py
@@ -1,5 +1,6 @@
 import discord
 from datetime import datetime as dt
+import pytz
 
 from discord.ext import commands
 
@@ -20,9 +21,18 @@ async def games(ctx):
     await ctx.send("<@191661345794424832> <@329167323510603786> <@537543052982485013> it gaem time. \nSincerely, Robot")
 
 @client.command()
-async def mel_time(ctx):
-    now = dt.now()
-    current_time = now.strftime("%H:%H")
-    await ctx.send(current_time)
+async def times(ctx):
+    #now = dt.now()
+    #current_time = now.strftime("%H:%H")
+    MelTZ = pytz.timezone('Australia/Melbourne')
+    KiwiTZ = pytz.timezone('Pacific/Auckland')
+    CowTZ = pytz.timezone('Australia/Queensland')
+
+    MelTime = dt.now(MelTZ).strftime('%#I:%M %p')
+    KiwiTime = dt.now(KiwiTZ).strftime('%#I:%M %p')
+    CowTime = dt.now(CowTZ).strftime('%#I:%M %p')
+
+    await ctx.send('The times are:\n**Actual Time**: {}\n**Kiwi Time**: {}\n**Cow Time**: {}\n\nSincerely, Robot'.format(MelTime, KiwiTime, CowTime))
+
 
 client.run('MTAzMjA0MTAxMDIyMzY0ODc5OA.GPgesj.bM8Qg19LcmmVXLsjuvGtyh292HUEaetJjhuFtI')
