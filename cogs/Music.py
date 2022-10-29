0f16f5fef6b019179f096b90cac8c01ca3deca8e
Robbie Le Grange <robbie.legrange@gmail.com>
2022-10-29 20:16:46 +1100

    Added gather_songs command

--- cogs/Music.py
+++ cogs/Music.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 from discord import FFmpegPCMAudio
-
+import os
 
 
 
@@ -19,7 +19,7 @@ class Music(commands.Cog):
     @commands.command()
     async def play(self, ctx, arg):
         extension = '.mp3'
-        song = arg + extension
+        song = './Music' + arg + extension
         source = FFmpegPCMAudio(song)
         voice = ctx.guild.voice_client
         player = voice.play(source, after=lambda x=None: check_queue(ctx, ctx.guild.id))
@@ -48,6 +48,28 @@ class Music(commands.Cog):
             voice = ctx.guild.voice_client
             source = queues[id].pop(0)
             player = voice.play(source)
+
+    async def create_path(self, parent, arg):
+        path_list = [parent]
+        path_list.extend(list(arg))
+        path = '/'.join(path_list)
+        return path
+
+    @commands.command()
+    async def gather_songs(self, ctx, *arg):
+        parent = './Music'
+        path =  await self.create_path(parent, arg)
+        folder_list, song_list = [], []
+        for root, dirs, files in os.walk(path):
+            folder_list.extend(dirs)
+            song_list.extend(files)
+            break
+        for song in song_list:
+            if not song.endswith('.mp3'):
+                song_list.remove(song)
+        song_list = [s.replace('.mp3','') for s in song_list]
+        await ctx.send('Folder List:\n{} '.format(folder_list))
+        await ctx.send('Song List:\n{} '.format(song_list))
 
     @commands.command()
     async def queue(self, ctx, arg):
